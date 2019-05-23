ce06d71dfe0f03145c81f142ad4e30e1462023af
Flávio Filipe <flaviofilipe97@gmail.com>
2019-05-22 23:22:36 -0300

    Criando menu na linha de comando

--- boot.py
+++ boot.py
@@ -1,9 +1,28 @@
 # https://python-googlesearch.readthedocs.io/en/latest/
 from googlesearch import search, get_random_user_agent
 
-def findSites(query, estado='', qnt=None, site='gov.br'):
+def findSites(query, qnt=None, site=''):
+    #site = site if site != '' else 'gov.br'
+    site = [site if site else 'gov.br']
     for result in search(
-            query, lang='pt', num=20, start=0, stop= int(qnt) if qnt else None, pause=2,
+            query, lang='pt', num=20, start=0, stop= int(qnt) if qnt else None, pause=1,
             domains=site,
             user_agent=get_random_user_agent()):
         yield result
+
+def searchAll():
+    query = input("Pesquisar: ")
+    qnt = input("Quantidade de itens (em branco para exibir todos): ")
+    return findSites(query, qnt)
+    
+def getSite():
+    site = input('Site: ')
+    if site[-6:] != 'gov.br':
+        print("\nOpss!\nInforme um site governamental (ex: site.gov.br) ou pesquise em todos")
+        return
+    query = input('Pesquisar: ')
+    quant = input("Quantidade de itens (em branco para exibir todos): ")
+    return findSites(query, quant, site)
+
+def getDocuments():
+    return ['Disponívem em breve']
--- main.py
+++ main.py
@@ -1,14 +1,40 @@
+# coding: utf-8
 import boot
 
-query = input("Pesquisar sites do governo: ")
-estado = input("Estado especififico (ex: ba): ")
-quant = input("Quantidade de itens (em branco para exibir todos): ")
-site = [estado + '.gov.br' if estado else 'gov.br']
 
 def main():
-    results = boot.findSites(query, estado, quant, site)
-    for result in results:
-        print(result)
+    print('''
+        ############################
+        # BEM VINDO AO SERENISSIMA #
+        ############################
+
+        Escolha uma opção
+        1- Pesquisar em sites do governo
+        2- Pesquisar site especifico (ex: site.gov.br)
+        3- Pesquisar documentos (ex: pdf, img, etc.)
+        0- Cancelar
+        ''')
+
+    option = input(">> ")
+
+    if option == '1':
+        for result in boot.searchAll():
+            print(result)
+        
+    elif option == '2':
+        for result in boot.getSite():
+                print(result)
+        
+    elif option == '3':
+        for result in boot.getDocuments():
+                print(result)
+        
+    elif option == '0':
+        print('Até mais!')
+    else:
+        print('Opção inválida')
+        main()
+
 
 if __name__ == "__main__":
     main()
